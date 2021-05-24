04154fbc15197ba4cf26a2068cfa2a9b7773f394
Mdsujansarkar <sujansarkar786@gmail.com>
2021-05-24 18:06:11 +0600

    db connect

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,24 @@
 <?php 
 class Database
 {
-	private $host		= '';
-	private $user_name	= '';
-	private $db_name	= '';
+	private $host		= 'localhost';
+	private $user_name	= 'root';
+	private $db_name	= 'testApi';
 	private $password 	= '';
-	private $conn		= '';
+	private $conn;
+
+	public function connect()
+	{
+		$this->conn = null;
+		
+		try {
+			$this->conn = new PDO('mysql:host='. $this->host .';dbname= ' . $this->db_name, $this->user_name,$this->password );
+			 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		} catch (PDOException $e) {
+			echo "Connection Error" .$e->getMessage();	
+		}
+
+		return $this->conn;
+	}
 }
